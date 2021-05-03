19dccff6ac1f3863f0d8356e31a3782a52452f6a
Mackenzie Chisholm <chisholm.mackenzie@gmail.com>
2021-05-03 19:41:13 -0400

    updated model associations

--- models/index.js
+++ models/index.js
@@ -1,6 +1,32 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
+// A user has many blog posts and a blog post has one user
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
 
-// A user has many blog posts but a blog post has one user
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
 
-// A blog post has many comments but a comment has one user
+// A blog post has many comments and a comment has one post
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
 
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+// A user has many comments and a comment has one user
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+module.exports = {User, Post, Comment}
